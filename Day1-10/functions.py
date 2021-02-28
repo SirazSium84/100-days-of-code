3897213c4f5f88d4de1aae0578efd8708768b87b
SirazSium84 <szsium@gmail.com>
2021-02-28 14:07:00 +0600

    Update

---
+++ Day1-10/functions.py
@@ -0,0 +1,67 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# for x in range(6):
+#     jump()
+
+
+# while not at_goal():
+#     jump()
+
+
+# hurldes-3
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     else:
+#         jump()
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     turn_left()
+#     while not right_is_clear():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     else:
+#         jump()
