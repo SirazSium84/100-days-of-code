1f1bf1f55f9953fa96f416bef1cc4d8c0370dd10
SirazSium84 <szsium@gmail.com>
2021-03-02 03:09:01 +0600

    Day 7

--- Day1-10/hangman.py
+++ Day1-10/hangman.py
@@ -1,23 +1,33 @@
-# Basic Algorithm....time to make it shiny
-# word = "happy"
-# len_word = len(word)
-# count = len_word
-# blank_word = "_" * len_word
-# print(blank_word)
-# while blank_word.count("_") != 0 and count != 0:
-#     letter = input("Give a letter: ").lower()
-#     if letter in word:
-#         position = word.index(letter)
-#         word = word[:position] + "_" + word[position+1:]
-#         blank_word = blank_word[:position] + letter + blank_word[position+1:]
-#         print(blank_word)
-#         count -= 1
-
-
 import random
-words_list = ["ardvark", "baboon", "camel"]
-chosen_word = random.choice(words_list)
+from hangman_art import stages, logo
+from hangman_words import word_list
+chosen_word = random.choice(word_list)
+print(logo)
 print(chosen_word)
-guess = input("Guess a letter: ").lower()
-if guess in chosen_word:
-    print("yes")
+display = []
+lives = 6
+end_of_game = False
+for x in range(len(chosen_word)):
+    display.append("_")
+while end_of_game == False:
+    guess = input("Guess a letter: ").lower()
+    if guess in chosen_word:
+        if guess not in display:
+            for position in range(len(chosen_word)):
+                letter = chosen_word[position]
+                if letter == guess:
+                    display[position] = letter
+            print(f"{''.join(display)}")
+        else:
+            print(f"You have already guessed {guess}")
+    else:
+        print(f"You guessed {guess}, that's not in the word. You lose a life")
+        print(f"{''.join(display)}")
+        print(stages[lives])
+        lives -= 1
+    if display.count("_") == 0:
+        end_of_game = True
+        print("You won!")
+    elif lives == -1:
+        print("You lost!")
+        end_of_game = True
