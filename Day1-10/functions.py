1410b928e0252754052989dcc050e09553ea8842
SirazSium84 <szsium@gmail.com>
2021-03-01 21:54:56 +0600

    Update

--- Day1-10/functions.py
+++ Day1-10/functions.py
@@ -48,17 +48,39 @@
 #     turn_left()
 #     turn_left()
 #     turn_left()
-    
+
+# Hurdles-4
 # def jump():
 #     turn_left()
 #     while not right_is_clear():
 #         move()
+
 #     turn_right()
 #     move()
 #     turn_right()
 #     while front_is_clear():
 #         move()
 #     turn_left()
+
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     else:
+#         jump()
+
+
+# Final Maze
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     else:
+#         turn_left()
 
 # while not at_goal():
 #     if front_is_clear():
