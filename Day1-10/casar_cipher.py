09c4aaa140f45daecc58c45ff097a2101c46b949
SirazSium84 <szsium@gmail.com>
2021-03-18 01:11:01 +0600

    Updating

---
+++ Day1-10/casar_cipher.py
@@ -0,0 +1,7 @@
+def greet(name_1, name_2, name_3):
+    print(f"Welcome to our house {name_1}!")
+    print(f"Please make yourself comfortable {name_2}.")
+    print(f"I hope you had a great time {name_3}.")
+
+
+greet(name_3="Sium", name_2="Sunan", name_1="Nazneen")
