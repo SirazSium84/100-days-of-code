94b9c1a625edb796d49fc429182c13c3923f47ef
SirazSium84 <szsium@gmail.com>
2021-05-27 18:59:16 +0600

    Updating the files

--- Day1-100/Day1-10/simple_calculator.py
+++ Day1-100/Day1-10/simple_calculator.py
@@ -1,3 +1,11 @@
+from os import system, name
+from calculator_art import logo
+
+
+def clear():
+    system("clear") if name == "posix" else system("cls")
+
+
 def add(a, b):
     return a + b
 
@@ -14,33 +22,40 @@ def divide(a, b):
     return a / b
 
 
-operations = ["+", "-", "*", "/"]
+operations = {"+": add,
+              "-": sub,
+              "*": multiply,
+              "/": divide}
 result = {"first_number": None}
-while True:
-    if result["first_number"] is None:
-        result["first_number"] = int(input("What's the first number: "))
-        output = 0
-    for x in operations:
-        print(x)
-    operator = input("Pick an operation: ")
-    result["next_number"] = int(input("What's the next number?: "))
-    if operator == "+":
-        output += add(result["first_number"], result["next_number"])
-    elif operator == "-":
-        output += sub(result["first_number"], result["next_number"])
-    elif operator == "*":
-        output += multiply(result["first_number"], result["next_number"])
-    elif operator == "/":
-        output += divide(result["first_number"], result["next_number"])
-    print(
-        f"{result['first_number']} {operator} {result['next_number']} = {output}")
-    decision = input(
-        f"Type 'y' to continue calculating with {output} or type 'n' or type 'e' to end calculator: ")
-    if decision == "y":
-        result["first_number"] = output
-        output = 0
-        continue
-    elif decision == "n":
-        result["first_number"] = None
-    else:
-        break
+
+
+def calculator():
+    print(logo)
+    while True:
+        if result["first_number"] is None:
+            result["first_number"] = int(input("What's the first number: "))
+            output = 0
+        for symbol in operations:
+            print(symbol)
+        operator = input("Pick an operation from the line above: ")
+        result["next_number"] = int(input("What's the next number?: "))
+        calculation_function = operations[operator]
+        answer = calculation_function(
+            result["first_number"], result["next_number"])
+        print(
+            f"{result['first_number']} {operator} {result['next_number']} = {answer}")
+        decision = input(
+            f"Type 'y' to continue calculating with {answer} or type 'n' to start a new calculation or type 'e' to end calculator: ")
+        if decision == "y":
+            result["first_number"] = answer
+            output = 0
+            continue
+        elif decision == "n":
+            result["first_number"] = None
+            clear()
+        else:
+            print("Have a good day. Thanks!")
+            break
+
+
+calculator()
